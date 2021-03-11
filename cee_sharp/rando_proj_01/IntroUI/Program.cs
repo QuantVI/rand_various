6f6d1ac597ccfe8fbcb022da34f57932a31621b0
QuantVI <berkeleyvi@gmail.com>
2021-03-11 11:48:00 +0100

    moving c# folder here

---
+++ cee_sharp/rando_proj_01/IntroUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = "Vincent",
+                LastName = "Ibia"
+            };
+
+            System.Console.WriteLine($"{ p.FirstName } { p.LastName } is my name.");
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test");
+            Console.WriteLine("The End");
+        }
+    }
+}
